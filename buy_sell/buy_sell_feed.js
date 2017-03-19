b173aa79e028b64ae8ad7cef0d365c8b262529d5
Skand <pvskand@gmail.com>
2017-03-19 13:00:26 +0530

    added feature of viewing image

--- buy_sell/buy_sell_feed.js
+++ buy_sell/buy_sell_feed.js
@@ -7,7 +7,7 @@ function tableCreate(body, tbl)
 
     for(var i = 0; i < 1; i++){
         var tr = tbl.insertRow();
-        for(var j = 0; j < 6; j++)
+        for(var j = 0; j < 7; j++)
         {
 
             var td = tr.insertCell();
@@ -23,11 +23,23 @@ function tableCreate(body, tbl)
         		td.appendChild(document.createTextNode("Cost"));
         	else if (j==5)
         		td.appendChild(document.createTextNode("Phone Number"));
+        	else if (j==6)
+        		td.appendChild(document.createTextNode("Image"));
             td.style.border = '1px solid black';
             
         }
     }
     body.appendChild(tbl);
+}
+
+function show_image(src, width, height) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
 }
 
 
@@ -54,10 +66,11 @@ $(document).ready(function()
 						item = data[i]["item"]; 
 						desc = data[i]["description"];
 						cost = data[i]["cost"];
-						ph_no = data[i]["phone_number"];   
+						ph_no = data[i]["phone_number"];
+						image_name =  data[i]["image_name"];  
 
 					var tr = tbl.insertRow();
-			        for(var j = 0; j < 6; j++)
+			        for(var j = 0; j < 7; j++)
 			        {
 			           
 		                var td = tr.insertCell();
@@ -73,6 +86,28 @@ $(document).ready(function()
 		            		td.appendChild(document.createTextNode(cost));
 		            	else if (j==5)
 		            		td.appendChild(document.createTextNode(ph_no));
+		            	else if (j==6)
+		            	{
+		            			
+		            		var oImg = document.createElement("img");
+		            		var path = "sell_upload/" + image_name;
+					        oImg.setAttribute('src', path);
+					        oImg.width = '100';
+					        oImg.height = '100';
+					        td.appendChild(oImg);
+
+
+
+
+
+
+		            		
+		            		// var img = "See Image";
+		            			
+		            		// td.appendChild(document.createTextNode(img));
+		            		// td.href = path;
+		            		// td.title = img;
+		            	}
 		                td.style.border = '1px solid black';  
 			        }
 					}
