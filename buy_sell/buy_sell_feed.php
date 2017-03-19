5ecfb1d130dceab045374a6453dbc4439210f50a
Skand <pvskand@gmail.com>
2017-03-19 13:00:46 +0530

    retreiving image from db

--- buy_sell/buy_sell_feed.php
+++ buy_sell/buy_sell_feed.php
@@ -12,7 +12,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 $sql ="SELECT * FROM sell_table";
 $result = mysqli_query($conn , $sql);
-file_put_contents('file.txt', "successfull");
 
 $data = array();	
 $count = 0;
@@ -28,6 +27,23 @@ if(mysqli_num_rows($result)) {
 		$count += 1;
 	}
 }
+
+$sql1 ="SELECT * FROM sell_image";
+
+$result1 = mysqli_query($conn , $sql1);
+$count1 = 0;
+if(mysqli_num_rows($result1)) {
+	while($row = mysqli_fetch_assoc($result1)) {
+		if($count1 == $count)
+			break;
+
+		$data[$count1]["image_name"] = $row["image_name"]; 
+		$count1 += 1;
+	}
+}
+
+
+
 echo json_encode($data);
 
 
