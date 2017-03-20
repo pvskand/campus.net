fc4feddcee4cce9aa4059836944ca1cfa503da88
Skand <pvskand@gmail.com>
2017-03-20 23:32:36 +0530

    added homepage

---
+++ homepage/index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+
+
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>HomePage</title>
+    <link rel="stylesheet" type="text/css" href="css/style_index.css"> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="index_feed.js"></script>
+  </head>
+  <body style="min-height: 200px">
+    <div class="fixed">
+      <ul>
+        <li><a href="index.php">Campus.net</a></li>
+        <li><a href="buy_sell.php">Buy & Sell</a></li>
+        <li><a href="news.asp">Lecture Hall Booking</a></li>
+        <li><a href="contact.asp">Question Papers</a></li>
+        <li><a href="about.asp">About</a></li>
+      </ul>
+
+    </div>
+
+<br>
+
+
+
+<div class="top-left" id = "tl">
+<h3> Events </h3>
+</div>
+<div class="top-right" id="tr">
+<h3> Books Issued </h3>
+</div>
+<div class="bottom-left" id = "bl">
+  <div id = "bf">
+      <h3> Breakfast </h3>
+
+  </div>
+
+  <div id = "lunch">
+  <br>
+      <h3> Lunch </h3>
+
+  </div>
+
+  <div id = "dinner">
+  <br>
+      <h3> Dinner </h3>
+
+  </div>
+
+</div>
+<div class="bottom-right" id = "br">
+<h3> Attendance </h3>
+</div>
+
+  </body>
+</html>
+
